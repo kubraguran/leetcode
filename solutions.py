91731d5c280f8bbd1571287b1f69aa323db6cad5
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-08-14 15:06:16 +0200

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -9,3 +9,13 @@ class Solution(object):
                     result =  [n , m]
 
         return result
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+       string = str(x)[::-1]
+       result = True if str(x) == string else False
+       return result
+       
+     
+     
