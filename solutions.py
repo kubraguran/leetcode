b656dd5665e075bc40eeba7f77fa625615c3ee3f
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-08-24 15:28:08 +0200

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -56,4 +56,22 @@ class Solution(object):
      return False 
 
 
+
+
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and abs(j - i) <= k:
+                    return True
+                
+        return False
+                   
+                
+        
+   
+
+        
         
