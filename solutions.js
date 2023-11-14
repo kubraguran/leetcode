db35be318848128a8b2170b805bc9169b14e5444
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-11-14 13:33:44 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1411,3 +1411,25 @@ var reverseString = function(s) {
     return arr.length;
 
 };
+
+
+
+  /**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+    let max = -1
+    
+    
+    for(let i = arr.length - 1; i > 0; i--){
+        let temp = arr[i]
+        arr[i] = max
+        max = Math.max(max,temp)
+    }
+    
+    return arr
+};
+
+
+  
