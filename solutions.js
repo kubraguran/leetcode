456d928833bd2717f3ce2bfca9757099827eb4cc
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-11-14 13:34:19 +0100

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1422,7 +1422,7 @@ var replaceElements = function(arr) {
     let max = -1
     
     
-    for(let i = arr.length - 1; i > 0; i--){
+    for(let i = arr.length - 1; i >= 0; i--){
         let temp = arr[i]
         arr[i] = max
         max = Math.max(max,temp)
